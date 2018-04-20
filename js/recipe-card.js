5b38503cd6225d8be1086e735d38d1aea32bd6b5
Linabel <32746012+Linabel@users.noreply.github.com>
2018-04-19 18:04:33 -0700

    Update recipe-card.js

--- js/recipe-card.js
+++ js/recipe-card.js
@@ -4,7 +4,7 @@ var recipe = {
     title: "Guacamole",
     servings: 4,
     ingredients: ["3 Avocados", "1 Lime", "1 Tsp Salt", "1/2 Cup of Onion", "3 Tbs Cilantro", "2 Diced Tomatoes", "1 Tsp Garlic", "1 Pinch Ground Pepper"],
-    printRecipe: function () {
+    getRecipe: function () {
         "use strict";
         var i;
         window.console.log(this.title + "\n" + "Serves: " + this.servings + "\n" + "Ingredients:");
@@ -13,4 +13,4 @@ var recipe = {
         }
     }
 };
-recipe.printRecipe();
+recipe.getRecipe();
